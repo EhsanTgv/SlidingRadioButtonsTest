e088d96452c6277f011b5b38286e5decce18013c
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-24 19:08:44 +0330

    MainActivity using custom views

--- app/src/main/java/com/taghavi/slidingradiobuttonstest/MainActivity.kt
+++ app/src/main/java/com/taghavi/slidingradiobuttonstest/MainActivity.kt
@@ -2,10 +2,32 @@ package com.taghavi.slidingradiobuttonstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listOfOptions:List<String>  = listOf("one", "two", "one")
+
+        customRadioGroup.setOnCheckedChangeListener(null)
+        customRadioGroup.removeAllViews()
+        customRadioGroup.selectedRadioButton = CustomRadioButton(this)
+        for (i  in listOfOptions.indices) {
+            val customRadioButton = CustomRadioButton(this)
+            customRadioButton.id = View.generateViewId()
+            customRadioButton.order = i
+            customRadioButton.customRadioGroup = customRadioGroup
+            customRadioButton.text = listOfOptions[i]
+            customRadioGroup.addView(customRadioButton)
+        }
+        customRadioGroup.setOnCheckedChangeListener { radioGroup, clickedButton ->
+            val customRadioButton: CustomRadioButton = radioGroup.findViewById(clickedButton)
+            if (customRadioButton.isChecked) {
+                customRadioGroup.selectedRadioButton = customRadioButton
+            }
+        }
     }
 }
